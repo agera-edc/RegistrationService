5c9a76a657ad6f42658ae0ffe62cf37af2b86bb5
Alexandre Gattiker <algattik@microsoft.com>
2022-08-02 18:40:16 +0200

    Update RegistrationApiClientTest.java

--- system-tests/src/test/java/org/eclipse/dataspaceconnector/registration/client/RegistrationApiClientTest.java
+++ system-tests/src/test/java/org/eclipse/dataspaceconnector/registration/client/RegistrationApiClientTest.java
@@ -14,13 +14,19 @@
 
 package org.eclipse.dataspaceconnector.registration.client;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.javafaker.Faker;
+import okhttp3.OkHttpClient;
+import org.eclipse.dataspaceconnector.identityhub.client.IdentityHubClientImpl;
 import org.eclipse.dataspaceconnector.registration.cli.ClientUtils;
 import org.eclipse.dataspaceconnector.registration.client.api.RegistryApi;
+import org.eclipse.dataspaceconnector.spi.monitor.ConsoleMonitor;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
 import static org.eclipse.dataspaceconnector.registration.client.TestUtils.CLIENT_DID_WEB;
 
 @IntegrationTest
@@ -28,6 +34,7 @@ public class RegistrationApiClientTest {
     static final String API_URL = "http://localhost:8182/authority";
     static final Faker FAKER = new Faker();
     static RegistryApi api;
+    static IdentityHubClientImpl identityHubClient;
 
     String participantUrl = FAKER.internet().url();
 
@@ -35,11 +42,12 @@ public class RegistrationApiClientTest {
     static void setUpClass() {
         var apiClient = ClientUtils.createApiClient(API_URL, CLIENT_DID_WEB, TestKeyData.PRIVATE_KEY_P256);
         api = new RegistryApi(apiClient);
+        var okHttpClient = new OkHttpClient.Builder().build();
+        identityHubClient = new IdentityHubClientImpl(okHttpClient, new ObjectMapper(), new ConsoleMonitor());
     }
 
     @Test
     void listParticipants() {
-
         assertThat(api.listParticipants())
                 .noneSatisfy(p -> assertThat(p.getUrl()).isEqualTo(participantUrl));
 
@@ -47,5 +55,16 @@ public class RegistrationApiClientTest {
 
         assertThat(api.listParticipants())
                 .anySatisfy(p -> assertThat(p.getUrl()).isEqualTo(participantUrl));
+
+
+        await().atMost(10, SECONDS).untilAsserted(() -> {
+            var r = identityHubClient.getVerifiableCredentials("http://localhost:8181/api/identity-hub");
+            assertThat(r.succeeded()).isTrue();
+            assertThat(r.getContent()).anySatisfy(p -> {
+                System.err.println(p.getJWTClaimsSet());
+                System.err.println(participantUrl);
+                assertThat(p.getJWTClaimsSet().getSubject()).isEqualTo(CLIENT_DID_WEB);
+            });
+        });
     }
 }
